7ac620119179d62de05b14188ba59efa6deb8abe
Olanrewaju taiwo <107349861+kiddo4@users.noreply.github.com>
2023-11-26 23:00:33 +0100

    title added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <title>The operating system for you and your products - shoptinga</title>
+          
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
